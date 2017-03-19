dd7ab4114caa3b360290c6ad4c409b5b7fa8ab6f
Christopher Baek <baekc@amazon.com>
2017-03-18 22:02:59 -0700

    Convert server to listen for UDP instead of ARP

--- dashbuttonserver.py
+++ dashbuttonserver.py
@@ -4,6 +4,7 @@ from scapy.all import *
 
 
 CONFIGURATION = None
+DASH_BUTTON_MACS = []
 
 
 def main():
@@ -13,11 +14,14 @@ def main():
 
 def read_configuration(path):
     global CONFIGURATION
+    global DASH_BUTTON_MACS
 
     print 'Reading configuration'
 
     with open(path) as input_file:
         CONFIGURATION = json.load(input_file)
+
+    DASH_BUTTON_MACS += CONFIGURATION['buttons'].values()
 
 
 def start_server():
@@ -25,20 +29,30 @@ def start_server():
 
     print 'Dash Button Server listening on', interface
 
-    sniff(iface=str(interface), prn=handle_packet, filter='arp', store=0, count=0)
+    sniff(iface=str(interface), prn=handle_packet, filter='udp', lfilter=filter_dash_button_macs, store=0)
+
+
+def filter_dash_button_macs(packet):
+    global DASH_BUTTON_MACS
+
+    return packet.src in DASH_BUTTON_MACS
 
 
 def handle_packet(packet):
-    if ARP in packet: # ARP layer exists
-        if packet[ARP].op == 1: # who-has (request)
-            packet_mac = packet[ARP].hwsrc
+    if DHCP in packet:
+        options = packet[DHCP].options
 
-            if packet_mac == CONFIGURATION['buttons']['wilson_jones_dash_button']:
-                do_wilson_jones_dash_button_action()
-            elif packet_mac == CONFIGURATION['buttons']['repurpose_dash_button']:
-                do_repurpose_dash_button_action()
-            elif packet_mac == CONFIGURATION['buttons']['the_laundress_dash_button']:
-                do_the_laundress_dash_button_action()
+        for option in options:
+            if isinstance(option, tuple):
+                if 'requested_addr' in option:
+                    packet_mac = packet.src
+
+                    if packet_mac == CONFIGURATION['buttons']['wilson_jones_dash_button']:
+                        do_wilson_jones_dash_button_action()
+                    elif packet_mac == CONFIGURATION['buttons']['repurpose_dash_button']:
+                        do_repurpose_dash_button_action()
+                    elif packet_mac == CONFIGURATION['buttons']['the_laundress_dash_button']:
+                        do_the_laundress_dash_button_action()
 
 
 def do_wilson_jones_dash_button_action():
