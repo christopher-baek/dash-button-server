bd6cb954c2f9dc94348e6f6b7050c5940f9fde5b
Christopher Baek <baekc@amazon.com>
2017-03-18 21:38:35 -0700

    Externalize configuration

--- dashbuttonserver.py
+++ dashbuttonserver.py
@@ -1,20 +1,31 @@
+import json
+
 from scapy.all import *
 
 
-WILSON_JONES_DASH_BUTTON_MAC = '44:65:0d:f5:f1:0f'
-REPURPOSE_DASH_BUTTON_MAC = 'ac:63:be:49:8c:f0'
-THE_LAUNDRESS_DASH_BUTTON_MAC = '50:f5:da:e7:84:5a'
+CONFIGURATION = None
 
 
 def main():
-    print 'Dash Button Server Listening...'
-    sniff(iface='wlan0', prn=handle_packet2, filter='arp', store=0, count=0)
+    read_configuration('config.json')
+    start_server()
 
 
-def handle_packet2(packet):
-    if ARP in packet:
-        if packet[ARP].hwsrc == WILSON_JONES_DASH_BUTTON_MAC:
-            packet.show()
+def read_configuration(path):
+    global CONFIGURATION
+
+    print 'Reading configuration'
+
+    with open(path) as input_file:
+        CONFIGURATION = json.load(input_file)
+
+
+def start_server():
+    interface = CONFIGURATION['interface']
+
+    print 'Dash Button Server listening on', interface
+
+    sniff(iface=str(interface), prn=handle_packet, filter='arp', store=0, count=0)
 
 
 def handle_packet(packet):
@@ -22,12 +33,11 @@ def handle_packet(packet):
         if packet[ARP].op == 1: # who-has (request)
             packet_mac = packet[ARP].hwsrc
 
-            if packet_mac == WILSON_JONES_DASH_BUTTON_MAC:
-                packet.show()
+            if packet_mac == CONFIGURATION['buttons']['wilson_jones_dash_button']:
                 do_wilson_jones_dash_button_action()
-            elif packet_mac == REPURPOSE_DASH_BUTTON_MAC:
+            elif packet_mac == CONFIGURATION['buttons']['repurpose_dash_button']:
                 do_repurpose_dash_button_action()
-            elif packet_mac == THE_LAUNDRESS_DASH_BUTTON_MAC:
+            elif packet_mac == CONFIGURATION['buttons']['the_laundress_dash_button']:
                 do_the_laundress_dash_button_action()
 
 
